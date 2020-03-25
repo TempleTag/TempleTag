784f4e1b972c2e550450f67d05362ec5d3002ff5
Viet Pham <tug36669@temple.edu>
2020-03-25 11:12:18 -0400

    Added welcome screen after login

--- app/src/main/java/edu/temple/templetag/TempleTagActivity.java
+++ app/src/main/java/edu/temple/templetag/TempleTagActivity.java
@@ -1,5 +1,6 @@
 package edu.temple.templetag;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -9,22 +10,25 @@ import android.widget.TextView;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class TempleTagActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseUser firebaseUser;
     private DatabaseReference reference;
-    TextView welcome_textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_templetag);
 
-        welcome_textview = findViewById(R.id.welcome_textView);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
         mAuth = FirebaseAuth.getInstance();
         firebaseUser = mAuth.getCurrentUser();
@@ -34,5 +38,17 @@ public class TempleTagActivity extends AppCompatActivity {
     private void displayUsername(){
         reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid()).child("username");
 
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String txt_username = (String) dataSnapshot.getValue();
+                getSupportActionBar().setTitle("Welcome, " + txt_username);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
