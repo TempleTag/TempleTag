9e5806ebd914fabefc95871833afc99ccc9741f2
Viet Pham <tug36669@temple.edu>
2020-03-25 11:20:48 -0400

    Added logout option

--- app/src/main/java/edu/temple/templetag/TempleTagActivity.java
+++ app/src/main/java/edu/temple/templetag/TempleTagActivity.java
@@ -4,7 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.android.material.appbar.AppBarLayout;
@@ -50,5 +53,24 @@ public class TempleTagActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(TempleTagActivity.this, HomeActivity.class));
+                finish();
+                return true;
+        }
+
+        return false;
     }
 }
