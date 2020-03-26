264f4b0fc56c41ce48cedd4d79efcc1dfe35c9ef
Viet Pham <tug36669@temple.edu>
2020-03-26 11:47:21 -0400

    Pass user login information to user setting activity

--- app/src/main/java/edu/temple/templetag/TempleTagActivity.java
+++ app/src/main/java/edu/temple/templetag/TempleTagActivity.java
@@ -31,6 +31,7 @@ public class TempleTagActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser firebaseUser;
     private DatabaseReference reference;
+    private String txt_username, txt_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,12 +61,13 @@ public class TempleTagActivity extends AppCompatActivity {
     }
 
     private void displayUsername(){
-        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid()).child("username");
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
 
         reference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String txt_username = (String) dataSnapshot.getValue();
+                txt_username = (String) dataSnapshot.child("username").getValue();
+                txt_email = (String) dataSnapshot.child("email").getValue();
                 getSupportActionBar().setTitle("Welcome, " + txt_username);
             }
 
@@ -91,7 +93,12 @@ public class TempleTagActivity extends AppCompatActivity {
                 finish();
                 return true;
             case R.id.account_setting:
-                startActivity(new Intent(TempleTagActivity.this, UserSettingActivity.class));
+                Bundle bundle = new Bundle();
+                bundle.putString("username", txt_username);
+                bundle.putString("email", txt_email);
+                Intent intent = new Intent(TempleTagActivity.this, UserSettingActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
                 return true;
         }
         return false;
--- app/src/main/java/edu/temple/templetag/UserSettingActivity.java
+++ app/src/main/java/edu/temple/templetag/UserSettingActivity.java
@@ -2,6 +2,7 @@ package edu.temple.templetag;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +11,22 @@ import androidx.preference.PreferenceFragmentCompat;
 
 public class UserSettingActivity extends AppCompatActivity {
 
+    private String txt_username, txt_email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+        txt_username = getIntent().getExtras().getString("username");
+        txt_email = getIntent().getExtras().getString("email");
+
+        TextView textView = findViewById(R.id.username);
+        textView.setText(txt_username);
+
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.setting_container, new UserSettingActivity.SettingsFragment())
+                .replace(R.id.setting_container, new UserSettingActivity.SettingsFragment(txt_username, txt_email))
                 .commit();
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -25,12 +35,20 @@ public class UserSettingActivity extends AppCompatActivity {
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
+
+        private String txt_username, txt_email;
+
+        public SettingsFragment(String txt_username, String txt_email){
+            this.txt_email = txt_email;
+            this.txt_username = txt_username;
+        }
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
 
             EditTextPreference editTextPreference = findPreference(getString(R.string.email));
-            editTextPreference.setText("Hello");
+            editTextPreference.setText(txt_email);
         }
     }
 
