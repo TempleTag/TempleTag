86b4f404f3a1526178b3f14c1ac3287d2343ff4c
Viet Pham <tug36669@temple.edu>
2020-03-26 11:32:41 -0400

    Setup user setting page

--- app/src/main/java/edu/temple/templetag/UserSettingActivity.java
+++ app/src/main/java/edu/temple/templetag/UserSettingActivity.java
@@ -5,6 +5,8 @@ import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
+import androidx.preference.PreferenceFragmentCompat;
 
 public class UserSettingActivity extends AppCompatActivity {
 
@@ -12,10 +14,23 @@ public class UserSettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.setting_container, new UserSettingActivity.SettingsFragment())
+                .commit();
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            EditTextPreference editTextPreference = findPreference(getString(R.string.email));
+            editTextPreference.setText("Hello");
         }
     }
 
