e8049eb5c0c894d9f99d4aae24a8b8460d625873
Dillon Coffman <tug40394@temple.edu>
2020-03-28 21:10:25 -0400

    Refactored sign in and up code to match UML diagram from our design documents; Refactored our app to use Firestore instead of Firebase Realtime Database

--- app/src/main/java/edu/temple/templetag/LoginActivity.java
+++ app/src/main/java/edu/temple/templetag/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,45 +18,72 @@ import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
-    MaterialEditText email, password;
-    private FirebaseAuth mAuth;
+    private MaterialEditText email, password;
+    private FirebaseAuth firebaseAuth;
+    private TextView signUp;
+    private Button signUpBtn;
+    private Button signInBtn;
+    private Intent homeIntent;
+    private Intent signUpIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        signUp = findViewById(R.id.signUpText);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+        signInBtn = findViewById(R.id.loginBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
 
-        mAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        Button btn_login = findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(new View.OnClickListener() {
+        signInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String txt_email = email.getText().toString();
-                String txt_password = password.getText().toString();
-
-                if (txt_email.equals("") || txt_password.equals("")){
-                    //fail
-                    Toast.makeText(LoginActivity.this, "Please fill out all required fields!", Toast.LENGTH_SHORT).show();
-                } else {
-                    mAuth.signInWithEmailAndPassword(txt_email, txt_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                                Intent intent = new Intent(LoginActivity.this, TempleTagActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                startActivity(intent);
-                                finish();
-                            } else {
-                                //fail
-                            }
-                        }
-                    });
-                }
+                signIn();
             }
         });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignUpActivity();
+            }
+        });
+    }
+
+    private void signIn() {
+        String txt_email = email.getText().toString();
+        String txt_password = password.getText().toString();
+
+        if (txt_email.equals("") || txt_password.equals("")){
+            Toast.makeText(LoginActivity.this, "Please fill in all required fields.", Toast.LENGTH_SHORT).show();
+        } else {
+            firebaseAuth.signInWithEmailAndPassword(txt_email, txt_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        startHomeActivity();
+                        finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Either your email or password did not match. Please try again.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void startHomeActivity() {
+        homeIntent = new Intent(LoginActivity.this, HomeActivity.class);
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(homeIntent);
+    }
+
+    private void startSignUpActivity() {
+        signUpIntent = new Intent(LoginActivity.this, SignUpActivity.class);
+        signUpIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(signUpIntent);
     }
 }
