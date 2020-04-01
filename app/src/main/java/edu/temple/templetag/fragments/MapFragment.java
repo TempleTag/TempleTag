9b856a4600f3b774500a64e88b0ce1ccc00c4cc5
Viet Pham <tug36669@temple.edu>
2020-04-01 13:20:23 -0400

    MapFragment displays tags that are close to currentUser location

--- app/src/main/java/edu/temple/templetag/fragments/MapFragment.java
+++ app/src/main/java/edu/temple/templetag/fragments/MapFragment.java
@@ -1,5 +1,9 @@
 package edu.temple.templetag.fragments;
 
+import android.app.Application;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.os.Bundle;
 
@@ -10,27 +14,38 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
+import edu.temple.templetag.HomeActivity;
 import edu.temple.templetag.R;
 import edu.temple.templetag.Tag;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
     private static final String ARG_PARAM1 = "tags";
     private static final String ARG_PARAM2 = "currentLocation";
+    private static final double MAX_RADIUS = 10;
+    private Location currentLocation;
 
     // TODO: Rename and change types of parameters
     private ArrayList<Tag> tags;
-    private LatLng currentUserLocation;
 
     //Initiate Map Stuff
     MapView mapView;
@@ -75,22 +90,60 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         return v;
     }
 
-    public void updateMap(ArrayList<Tag> tags){
-        if (googleMap != null) {
+    public void displayMarkers(ArrayList<Tag> Tags, Location currentLocation){
+        this.currentLocation = currentLocation;
+
+        if (googleMap != null && currentLocation != null) {
             googleMap.clear();
-            for (int i = 0; i < tags.size(); i++) {
-                Tag thisTag = tags.get(i);
-                LatLng loc = new LatLng(thisTag.getmTagLocationLat(), thisTag.getmTagLocationLong());
-                googleMap.addMarker(new MarkerOptions().position(loc).title(thisTag.getmTagDescription()));
+            for (Tag tag : Tags){
+                if (distanceFromTo(tag.getmTagLocationLat(), tag.getmTagLocationLong(), currentLocation.getLatitude(),
+                        currentLocation.getLongitude()) < MAX_RADIUS){ //if tag is close to the user --> display
+                    LatLng latLng = new LatLng(tag.getmTagLocationLat(), tag.getmTagLocationLong());
+                    googleMap.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .title(tag.getmTagLocationName())
+                            .snippet(tag.getmTagDescription())
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                }
             }
+            //Create user location marker
+            LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+            googleMap.addMarker(new MarkerOptions()
+                    .position(latLng)
+                    .title("You are here!")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+            focusOn(currentLocation);
         }
     }
 
-    public void focusOn(LatLng currentUserLocation){
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(currentUserLocation, 15);
-        googleMap.animateCamera(cameraUpdate);
+    private double distanceFromTo(double lat1, double lon1, double lat2, double lon2){
+        lon1 = Math.toRadians(lon1);
+        lon2 = Math.toRadians(lon2);
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        // Haversine formula
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        double a = Math.pow(Math.sin(dlat / 2), 2)
+                + Math.cos(lat1) * Math.cos(lat2)
+                * Math.pow(Math.sin(dlon / 2),2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+
+        // Radius of earth in kilometers. Use 3956
+        // for miles
+        double r = 3956;
+
+        // calculate the result
+        return(c * r);
     }
 
+    public void focusOn(Location location){
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
+        googleMap.animateCamera(cameraUpdate);
+    }
 
 
     @Override
