125b58945c2dc06b57fb36197b7f79496778246c
Viet Pham <tug36669@temple.edu>
2020-04-02 15:40:43 -0400

    Add TagRecyclerFragment in UserProfileActivity

--- app/src/main/java/edu/temple/templetag/UserProfileActivity.java
+++ app/src/main/java/edu/temple/templetag/UserProfileActivity.java
@@ -4,9 +4,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,13 +27,20 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+
 import edu.temple.templetag.fragments.TagRecyclerViewFragment;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     private String txt_username, txt_email;
     private TextView change_username_textview, username_textview, email_textview, tagcount_textview;
-    private String dialogText = "nothing";
+    private String dialogText;
+    final FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+    final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    private ArrayList<Tag> Tags = new ArrayList<>();
+    public static final String TAG_LIST_FRAGMENT = "TagRecyclerFragment_USER";
+    TagRecyclerViewFragment tagRecyclerViewFragment;
 
 
     @Override
@@ -39,7 +48,10 @@ public class UserProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile_activity);
 
+        //Display greeting + username
         displayInfo();
+        //Init textviews and buttons
+        tagcount_textview = findViewById(R.id.tagCount);
 
         findViewById(R.id.btn_lougout).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,30 +71,7 @@ public class UserProfileActivity extends AppCompatActivity {
         change_username_textview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(UserProfileActivity.this);
-                builder.setTitle("New Username");
-
-                // Set up the input
-                final EditText input = new EditText(UserProfileActivity.this);
-                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-                input.setInputType(InputType.TYPE_CLASS_TEXT);
-                builder.setView(input);
-
-                // Set up the buttons
-                builder.setPositiveButton("Change", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialogText = input.getText().toString();
-                        changeUsername();
-                    }
-                });
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                builder.show();
+                OnClickChangeCurrentUsername();
             }
         });
     }
@@ -99,9 +88,6 @@ public class UserProfileActivity extends AppCompatActivity {
 
     private void changeUsername(){
         //Go through firebase and change username
-        final FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
         firestore.collection("Users")
         .get()
         .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -119,7 +105,7 @@ public class UserProfileActivity extends AppCompatActivity {
                         reference.update("username", dialogText).addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
-                                username_textview.setText(dialogText);
+                                username_textview.setText("Hi, " + dialogText);
                             }
                         });
                     } else {
@@ -138,11 +124,93 @@ public class UserProfileActivity extends AppCompatActivity {
         email_textview = findViewById(R.id.emailTextView);
         email_textview.setText(txt_email);
         tagcount_textview = findViewById(R.id.tagCount);
-
-        tagcount_textview.setText(String.valueOf(getTagCount()) + " active tags");
+        tagcount_textview.setText(getTagCount() + " active tags");
     }
 
     private int getTagCount(){
-        return 0;
+        return Tags.size();
+    }
+
+    private void displayMyTagListFragment() {
+        new Thread() {
+            public void run() {
+                firestore.collection("Tags")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Tags.clear();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                if (document.getData().get("createdById").equals(firebaseUser.getUid())) {
+                                    double mTagLocationLatitude = (Double) document.getData().get("locationLat"); // we set based off user's current location
+                                    double mTagLocationLongitude = (Double) document.getData().get("locationLong"); // we set based off user's current location
+                                    String mTagID = document.getData().get("id").toString(); // need to generate a unique tag ID for each tag
+                                    String mTagDuration = document.getData().get("duration").toString(); // current Date tag was created, used to compare two dates later to determine which Tags get deleted in the database and which don't
+                                    String mTagImage = document.getData().get("imageRef").toString(); // user has option to take a picture to represent the tag, otherwise we show no image (null)
+                                    String mTagDescription = document.getData().get("description").toString(); // user sets when creating describing what's in the tag
+                                    String mTagLocationName = document.getData().get("locationName").toString(); // user sets when creating, naming the tag location
+                                    int mTagUpvoteCount = Integer.valueOf(document.getData().get("upvoteCount").toString()); // changes as other users upvote
+                                    int mTagDownvoteCount = Integer.valueOf(document.getData().get("downvoteCount").toString()); // changes as other users downvote
+                                    int mTagPopularity = Integer.valueOf(document.getData().get("popularityCount").toString()); // if multiple users tag the same location, we increase the marker size on the map using this field
+                                    String mTagCreatedBy = document.getData().get("createdBy").toString(); // name of user that created the tag
+                                    String mTagCreatedById = document.getData().get("createdById").toString();
+
+                                    Tag tag = new Tag(mTagID, mTagLocationName, mTagDuration, mTagImage, mTagDescription, mTagLocationLatitude,
+                                            mTagLocationLongitude, mTagUpvoteCount, mTagDownvoteCount, mTagPopularity, mTagCreatedBy, mTagCreatedById);
+                                    Tags.add(tag);
+                                }
+                            }
+                            //Create tag recycler list fragment after fetching tags
+                            tagRecyclerViewFragment = (TagRecyclerViewFragment) getSupportFragmentManager().findFragmentByTag(TAG_LIST_FRAGMENT);
+                            if (null != tagRecyclerViewFragment) {
+                                tagRecyclerViewFragment.updateDataSet(Tags);
+                            } else {
+                                tagRecyclerViewFragment = TagRecyclerViewFragment.newInstance(Tags);
+                                getSupportFragmentManager().beginTransaction()
+                                        .add(R.id.tag_recycler_fragment_container, tagRecyclerViewFragment, TAG_LIST_FRAGMENT)
+                                        .commit();
+                            }
+                        } else {
+                            Toast.makeText(UserProfileActivity.this, "Error getting Tags", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        }.start();
+    }
+
+    private void OnClickChangeCurrentUsername(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(UserProfileActivity.this);
+        builder.setTitle("New Username");
+
+        // Set up the input
+        final EditText input = new EditText(UserProfileActivity.this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("Change", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialogText = input.getText().toString();
+                changeUsername();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        displayMyTagListFragment();
     }
 }
