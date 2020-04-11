9eeb27fc41eb5e949fd1885e26a49e1e04c08883
Zach Essel <tuk01401@temple.edu>
2020-04-11 14:58:16 -0400

    in progress Add image display to TagDetailActivity

--- app/src/main/java/edu/temple/templetag/TagDetailActivity.java
+++ app/src/main/java/edu/temple/templetag/TagDetailActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -76,6 +78,8 @@ public class TagDetailActivity extends AppCompatActivity {
          *
          *
          * **/
+        Log.d("mTagImageURI = ", mTag.getmTagImageURI());
+        Picasso.with(this).load(mTag.getmTagImageURI()).into(tagImageView);
 
         mapFragment = (MapFragment) getSupportFragmentManager().findFragmentByTag(MAP_FRAG_IN_DETAIL);
 
