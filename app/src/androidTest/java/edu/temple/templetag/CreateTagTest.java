071a5727fa5fd13535f6eae52853e3ddecedde5b
Zach Essel <tuk01401@temple.edu>
2020-04-26 16:24:29 -0400

    Add testing dependencies and a couple tests

---
+++ app/src/androidTest/java/edu/temple/templetag/CreateTagTest.java
@@ -0,0 +1,83 @@
+package edu.temple.templetag;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.provider.MediaStore;
+
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.GrantPermissionRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CreateTagTest {
+
+    @Rule
+    public IntentsTestRule<CreateTagActivity> intentsTestRule =
+            new IntentsTestRule<>(CreateTagActivity.class);
+
+    @Rule
+    public GrantPermissionRule mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.ACCESS_FINE_LOCATION");
+
+    @Before
+    public void stubCameraIntent() {
+        Instrumentation.ActivityResult result = createImageCaptureActivityResultStub();
+
+        // Stub the Intent.
+        intending(hasAction(MediaStore.ACTION_IMAGE_CAPTURE)).respondWith(result);
+    }
+
+    @Test
+    public void createTagTest() {
+
+        onView(withId(R.id.tagLocationName))
+                .perform(typeText("Esp"), closeSoftKeyboard());
+        onView(withId(R.id.tagDescription))
+                .perform(typeText("Espresso Testing"), closeSoftKeyboard());
+        onView(withId(R.id.takeTagPictureBtn))
+                .perform(click());
+        onView(withId(R.id.createTagBtn))
+                .perform(click());
+
+    }
+
+    private Instrumentation.ActivityResult createImageCaptureActivityResultStub() {
+        // Put the drawable in a bundle.
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("data", BitmapFactory.decodeResource(
+                intentsTestRule.getActivity().getResources(), R.drawable.ic_popularity_foreground));
+
+        // Create the Intent that will include the bundle.
+        Intent resultData = new Intent();
+        resultData.putExtras(bundle);
+
+        // Create the ActivityResult with the Intent.
+        return new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
+    }
+}
